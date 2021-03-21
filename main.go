662755c392fbce633712e525b836266b34679ecd
Pedro F Linhares <pedro@profl.com.br>
2021-03-21 04:57:51 -0300

    :recycle: Split story arcs parsing for easier testing

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"embed"
-	"encoding/json"
 	"flag"
 	htmlTemplate "html/template"
 	"log"
-	"os"
 	textTemplate "text/template"
 
 	"github.com/ProFL/gophercises-cyoa/frontend"
-	"github.com/ProFL/gophercises-cyoa/model"
 )
 
 //go:embed static/* template/*
@@ -50,46 +47,4 @@ func main() {
 	}
 
 	app.Start(initialArc)
-}
-
-func parseStoryArcs(filePath string) (initialArc string, storyArcs map[string]model.StoryArc) {
-	var storyArcsJSON []byte
-	var err error
-	if filePath == "" {
-		storyArcsJSON, err = content.ReadFile("gopher.json")
-		if err != nil {
-			log.Panicln("Failed to load default story arcs file contents", err.Error())
-		}
-	} else {
-		storyArcsJSON, err = os.ReadFile(filePath)
-		if err != nil {
-			log.Panicln("Failed to load story arcs file contents", err.Error())
-		}
-	}
-
-	storyArcs = make(map[string]model.StoryArc)
-	err = json.Unmarshal(storyArcsJSON, &storyArcs)
-	if err != nil {
-		log.Panicln("Failed to parse story arcs file contents", err.Error())
-	}
-
-	initialArc = "intro"
-	_, ok := storyArcs[initialArc]
-	if !ok {
-		for arcName, storyArc := range storyArcs {
-			if storyArc.IsInitialStory {
-				initialArc = arcName
-				break
-			}
-		}
-		_, ok = storyArcs[initialArc]
-		if !ok {
-			log.Panicln(
-				"No initial story was found in the provided file!",
-				"It should have a isInitialStory key with true value",
-			)
-		}
-	}
-	log.Println("Initial arc:", initialArc)
-	return initialArc, storyArcs
 }
